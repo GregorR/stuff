556a1c00cbebb911cc6f302aed6fddf1ca5c32f7
Gregor Richards <Richards@codu.org>
2009-03-01 02:44:43 -0500

    Added more power, is now self-hosting (so to speak)

--- allindent.c
+++ allindent.c
@@ -8,6 +8,13 @@
 #define READBUFSZ       4096
 #define INDENTERS       "([{"
 #define UNINDENTERS     "}])"
+#define STRINGOPEN      "\""
+#define STRINGCLOSE     "\""
+#define STRINGESCAPE    "\\"
+#define COMMENTOPEN     "/*"
+#define COMMENTCLOSE    "*/"
+#undef  COMMENTESCAPE
+#define MAGICSTART      "#"
 #define WHITESPACE      " \t"
 
 struct Indenter {
@@ -21,12 +28,13 @@ int main(int argc, char **argv)
     FILE *inf, *outf;
     char *infname = NULL, *outfname = NULL;
     int i;
-    char *readbuf, *shortbuf, *outindent;
+    char *readbuf, *shortbuf;
 
     char indentc = ' ';
     int indentper = 4;
     struct Indenter *indenter, *lindent, *nindent;
     int indentation, lnum;
+    int instring, incomment, sinstring, sincomment;
 
     /* read the arguments */
     for (i = 1; i < argc; i++) {
@@ -44,12 +52,13 @@ int main(int argc, char **argv)
             }
 
             if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h")) {
-                fprintf(stderr, "Use: allindent [options] [input file] [output file]\n"
-                                "Options:\n"
-                                "  --indent-char|-c <character>: Character to use for indentation\n"
-                                "  --use-spaces|-s: Use ' ' for indentation (the default)\n"
-                                "  --use-tabs|-t: Use tabs for indentation\n"
-                                "  --indent-width|-w <number>: Width to indent (must come after -s or -t)\n");
+                fprintf(stderr,
+                    "Use: allindent [options] [input file] [output file]\n"
+                    "Options:\n"
+                    "  --indent-char|-c <character>: Character to use for indentation\n"
+                    "  --use-spaces|-s: Use ' ' for indentation (the default)\n"
+                    "  --use-tabs|-t: Use tabs for indentation\n"
+                    "  --indent-width|-w <number>: Width to indent (must come after -s or -t)\n");
                 return 0;
 
             } else if (!strcmp(argv[i], "--use-spaces") || !strcmp(argv[i], "-s")) {
@@ -95,6 +104,7 @@ int main(int argc, char **argv)
     SF(readbuf, malloc, NULL, (READBUFSZ));
     INIT_BUFFER(outb);
     indenter = NULL;
+    instring = incomment = 0;
 
     /* start reading */
     for (lnum = 0; fgets(readbuf, READBUFSZ, inf); lnum++) {
@@ -102,55 +112,124 @@ int main(int argc, char **argv)
         if (indenter) indentation = indenter->indentation;
         lindent = indenter;
 
+        /* did we start in a string/comment? */
+        sinstring = 0;
+        sincomment = 0;
+
         /* skip past the whitespace */
         for (i = 0; readbuf[i] && strchr(WHITESPACE, readbuf[i]); i++);
         shortbuf = readbuf + i;
 
-        /* look for indenters/unindenters */
+        /* check for a magic start */
+        if (!strncmp(shortbuf, MAGICSTART, sizeof(MAGICSTART)-1)) {
+            WRITE_BUFFER(outb, readbuf, strlen(readbuf));
+            continue;
+        }
+
+        /* look for indenters/unindenters, string/comment openers/closers */
         for (i = 0; shortbuf[i]; i++) {
-            if (strchr(INDENTERS, shortbuf[i])) {
-                /* indent further */
-                SF(nindent, malloc, NULL, (sizeof(struct Indenter)));
-                nindent->next = indenter;
-                nindent->linenum = lnum;
+            if (instring) {
+                /* did we close the string? */
+                if (!strncmp(shortbuf + i, STRINGCLOSE, sizeof(STRINGCLOSE)-1)) {
+                    instring--;
+                    if (instring < 0) instring = 0;
 
-                /* check the current indentation */
-                if (lindent) {
-                    if (lindent->linenum == lnum) {
-                        nindent->indentation = lindent->indentation;
+                } else if (!strncmp(shortbuf + i, STRINGOPEN, sizeof(STRINGOPEN)-1)) {
+                    instring++;
+
+#ifdef STRINGESCAPE
+                } else if (!strncmp(shortbuf + i, STRINGESCAPE, sizeof(STRINGESCAPE)-1)) {
+                    /* skip past it */
+                    i += sizeof(STRINGESCAPE) - 1;
+#endif
+
+                }
+
+            } else if (incomment) {
+                /* did we close the comment? */
+                if (!strncmp(shortbuf + i, COMMENTCLOSE, sizeof(COMMENTCLOSE)-1)) {
+                    incomment--;
+                    if (incomment < 0) incomment = 0;
+
+                } else if (!strncmp(shortbuf + i, COMMENTOPEN, sizeof(COMMENTOPEN)-1)) {
+                    incomment++;
+
+#ifdef COMMENTESCAPE
+                } else if (!strncmp(shortbuf + i, COMMENTESCAPE, sizeof(COMMENTESCAPE)-1)) {
+                    i += sizeof(COMMENTESCAPE) - 1;
+#endif
+
+                }
+
+            } else {
+                if (strchr(INDENTERS, shortbuf[i])) {
+                    /* indent further */
+                    SF(nindent, malloc, NULL, (sizeof(struct Indenter)));
+                    nindent->next = lindent;
+                    nindent->linenum = lnum;
+
+                    /* check the current indentation */
+                    if (lindent) {
+                        if (lindent->linenum == lnum) {
+                            nindent->indentation = lindent->indentation;
+                        } else {
+                            nindent->indentation = lindent->indentation + indentper;
+                        }
                     } else {
-                        nindent->indentation = lindent->indentation + indentper;
+                        nindent->indentation = indentper;
                     }
-                } else {
-                    nindent->indentation = indentper;
-                }
-                lindent = nindent;
-
-            } else if (strchr(UNINDENTERS, shortbuf[i])) {
-                /* unindent */
-                if (lindent) {
-                    if (indenter == lindent)
-                        indenter = indenter->next;
-                    nindent = lindent->next;
-                    free(lindent);
                     lindent = nindent;
-                }
 
-                if (indenter) {
-                    indentation = indenter->indentation;
-                } else {
-                    indentation = 0;
+                } else if (strchr(UNINDENTERS, shortbuf[i])) {
+                    /* unindent */
+                    if (lindent) {
+                        if (indenter == lindent) {
+                            indenter = indenter->next;
+                        }
+                        nindent = lindent->next;
+                        free(lindent);
+                        lindent = nindent;
+                    }
+
+                    /* move back IF this is the first thing on the line */
+                    if (i == 0) {
+                        if (indenter) {
+                            indentation = indenter->indentation;
+                        } else {
+                            indentation = 0;
+                        }
+                    }
+
+                } else if (!strncmp(shortbuf + i, STRINGOPEN, sizeof(STRINGOPEN)-1)) {
+                    instring++;
+
+                } else if (!strncmp(shortbuf + i, COMMENTOPEN, sizeof(COMMENTOPEN)-1)) {
+                    incomment++;
+
                 }
             }
         }
         indenter = lindent;
 
         /* now output */
-        SF(outindent, malloc, NULL, (indentation));
-        memset(outindent, indentc, indentation);
-        WRITE_BUFFER(outb, outindent, indentation);
-        free(outindent);
-        WRITE_BUFFER(outb, shortbuf, i);
+        if (sinstring || sincomment) {
+            /* we started in a string or comment, so don't change the indentation at all */
+            WRITE_BUFFER(outb, readbuf, strlen(readbuf));
+
+        } else if (i == 1) {
+            /* all this contained was the newline! Don't indent an empty newline */
+            WRITE_BUFFER(outb, shortbuf, i);
+
+        } else {
+            /* create the proper indentation */
+            while (BUFFER_SPACE(outb) < indentation) {
+                EXPAND_BUFFER(outb);
+            }
+            memset(BUFFER_END(outb), indentc, indentation);
+            STEP_BUFFER(outb, indentation);
+            WRITE_BUFFER(outb, shortbuf, i);
+
+        }
     }
     WRITE_BUFFER(outb, "\0", 1);
     fclose(inf);
@@ -159,7 +238,7 @@ int main(int argc, char **argv)
     if (outfname == NULL) {
         outf = stdout;
     } else {
-        SF(outf, fopen, NULL, (outfname, "r"));
+        SF(outf, fopen, NULL, (outfname, "w"));
     }
 
     fputs(outb.buf, outf);
