ea8cfb5f7e98092ee8ee1f15180a04b188ab3475
Gregor Richards <Richards@codu.org>
2009-05-02 00:47:54 -0400

    graphgame/: Now supports "tron-style" (multiple starting points, try not to hit the other person's line), added mkbook.sh which just generates 100 graphs and puts them in a pdf.

--- graphgame/graphgame.c
+++ graphgame/graphgame.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #include <gc/gc.h>
+
+#define ISMAGIC(_nodec, _node) ((_node) > 1 && (_node) <= (_nodec)/3+1)
 
 char *nodeName(int num)
 {
@@ -20,34 +24,71 @@ char *nodeName(int num)
     return name;
 }
 
+static inline int randomNonMagic(int nodec)
+{
+    int v;
+    do {
+        v = random() % (nodec - (nodec/3));
+    } while (ISMAGIC(nodec, v));
+    return v;
+}
+
 int main(int argc, char **argv)
 {
-    int nodec, edgec, i;
+    int nodec, edgec, i, f;
+    unsigned int seed;
 
-    if (argc != 3) {
-        fprintf(stderr, "Use: graphgame <number of nodes> <number of edges>\n");
+    if (argc < 3) {
+        fprintf(stderr, "Use: graphgame <number of nodes> <number of edges> [random seed]\n");
         return 1;
     }
 
     nodec = atoi(argv[1]);
     edgec = atoi(argv[2]);
 
-    srandom(time(NULL));
+    if (argc < 4) {
+        seed = time(NULL) ^ getpid();
+    } else {
+        seed = (unsigned) atoi(argv[3]);
+    }
+    srandom(seed);
 
     printf("digraph {\n"
-           "AA [label=\"\", style=\"filled\", bgcolor=\"#DDDDDD\"];\n");
-    for (i = 1; i < nodec; i++) {
+           "// nodes: %d, edges: %d, seed: %u\n"
+           "AA [label=\"\", shape=\"parallelogram\", style=\"filled\", bgcolor=\"#AAAAAA\"];\n"
+           "AB [label=\"\", shape=\"trapezium\", style=\"filled\", bgcolor=\"#AAAAAA\"];\n",
+           nodec, edgec, seed);
+    for (i = 2; i < nodec; i++) {
         printf("%s [label=\"\"%s];\n", nodeName(i),
-               (i <= (nodec/3)) ? ", shape=\"box\"" : "");
+               ISMAGIC(nodec, i) ? "" : ", shape=\"box\"");
     }
     for (i = 0; i < nodec && i < edgec; i++) {
         printf("%s -> %s [color=\"#%02X%02X%02X\"];\n",
-               nodeName(i), nodeName(random() % nodec),
+               nodeName(i),
+               nodeName(ISMAGIC(nodec, i) ?
+                   randomNonMagic(nodec) :
+                   random() % nodec),
                random() % 0x80, random() % 0x80, random() % 0x80);
     }
+    if (edgec >= nodec * 2) {
+        /* guarantee that every node has an input */
+        for (i = 0; i < nodec; i++) {
+            printf("%s -> %s [color=\"#%02X%02X%02X\"];\n",
+                   nodeName(ISMAGIC(nodec, i) ?
+                       randomNonMagic(nodec) :
+                       random() % nodec),
+                   nodeName(i),
+                   random() % 0x80, random() % 0x80, random() % 0x80);
+        }
+        i += nodec;
+    }
     for (; i < edgec; i++) {
+        f = random() % nodec;
         printf("%s -> %s [color=\"#%02X%02X%02X\"];\n",
-               nodeName(random() % nodec), nodeName(random() % nodec),
+               nodeName(f),
+               nodeName(ISMAGIC(nodec, f) ?
+                   randomNonMagic(nodec) :
+                   random() % nodec),
                random() % 0x80, random() % 0x80, random() % 0x80);
     }
     printf("}\n");
---
+++ graphgame/mkbook.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [ ! "$3" ]
+then
+    echo 'Use: mkbook.sh <number of nodes> <number of edges> <pages>' >&2
+    exit 1
+fi
+
+NODES="$1"
+EDGES="$2"
+PREFIX="${NODES}_${EDGES}"
+PAGES="$3"
+TITLE="GraphGame $NODES/$EDGES"
+
+# First generate them
+for (( i = 1; $i <= $PAGES; i++ ))
+do
+    echo $i >&2
+    if [ ! -e ${PREFIX}_${i}.dot ]
+    then
+        ./graphgame $NODES $EDGES $i > ${PREFIX}_${i}.dot
+    fi
+done
+
+echo '\documentclass{article} \usepackage{graphicx} \usepackage[landscape,margin=0in]{geometry} \begin{document}
+\title{\Huge{'${TITLE}'}} \date{} \maketitle \newpage' > ${PREFIX}_book.tex
+
+for (( i = 1; $i <= $PAGES; i++ ))
+do
+    make ${PREFIX}_${i}.dpdf >&2
+    mv -f ${PREFIX}_${i}.dpdf ${PREFIX}_${i}_dot.pdf
+
+    echo '\includegraphics[width=10.5in,height=8in]{'${PREFIX}'_'$i'_dot.pdf}\newpage' >> ${PREFIX}_book.tex
+done
+
+echo '\end{document}' >> ${PREFIX}_book.tex
+
+pdflatex ${PREFIX}_book.tex
+rm -f ${PREFIX}_book.{aux,log} ${PREFIX}_*_dot.pdf &&
+tar jcf ${PREFIX}_book.tar.bz2 ${PREFIX}_*.dot ${PREFIX}_*.pdf ${PREFIX}_book.tex &&
+rm -f ${PREFIX}_*.dot ${PREFIX}_*_dot.pdf ${PREFIX}_book.tex
